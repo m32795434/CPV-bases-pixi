9974568c7f5fa551f527637f748c96118b0e1472
Manuel Augusto Bravard <manubravard@gmail.com>
2024-03-10 10:52:35 -0300

    notes about toGlobal and toLocal

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,13 @@ const app = new Application();
 
 async function init() {
     // Initialize the application
-    await app.init({ background: '#1099bb', width: 1000, height: 1000 });
+    await app.init({ background: '#1099bb', width: 600, height: 600 });
 
     // Append the application canvas to the document body
     document.body.appendChild(app.canvas);
 
+    console.log("Init------\n canvas.width, canvas.height", app.canvas.width, app.canvas.height)
+    console.log("Init------\n screen.width, screen.height", app.screen.width, app.screen.height)
     // Manifest example
     // Assets.init must only happen once! 
     // Pack all your bundles into one manifest!
@@ -28,11 +30,13 @@ async function init() {
         const scaleY = window.innerHeight / app.screen.height
         const scale = Math.min(scaleX, scaleY)
 
-        const screenWidth = Math.round(app.screen.width * scale)
+        const screenWidth = Math.round(app.canvas.width * scale)//the same..app.canvas.width, app.screen.width are the initial values
         const screenHeight = Math.round(app.screen.height * scale)
 
         app.canvas.style.width = screenWidth + "px"
         app.canvas.style.height = screenHeight + "px"
+        console.log("resized------app.canvas.clientWidth:", app.canvas.clientWidth)
+        console.log("resized------app.canvas.clientHeight", app.canvas.clientHeight)
     })
     window.dispatchEvent(new Event('resize'))
 }
@@ -52,8 +56,6 @@ async function makeLoadScreen() {
     const clampy = Sprite.from(texture1);
 
 
-
-
     clampy.anchor.set(1)
     clampy.x = app.screen.width;
     clampy.y = app.screen.height;
@@ -61,44 +63,56 @@ async function makeLoadScreen() {
     clampy.scale.y = 0.5
     app.stage.addChild(clampy)
 
-    const goNextWithHat: Container = new Container();
+    const flowerToptWithHat: Container = new Container();
+    //all contanier's properties, FIRST
+    //flowerToptWithHat.position.set(40, 60)
 
     flowerTop.anchor.set(0.5);
     flowerTop.x = app.screen.width / 2;
     flowerTop.y = app.screen.height / 2;
     //we have .rotation, .angle, .scale too, and more!
-    goNextWithHat.addChild(flowerTop)
+    flowerToptWithHat.addChild(flowerTop)
 
-    const goNextStroke = new Graphics()
+    const flowerTopStroke = new Graphics()
         .rect((flowerTop.x - flowerTop.width / 2), (flowerTop.y - flowerTop.height / 2), flowerTop.width, flowerTop.height)
         .stroke(0x0000ff)
+    console.log("flowerTopStroke x: ", (flowerTop.x - flowerTop.width / 2))//300-119/2= 300-59.5 = 240.5
+    console.log("flowerTopStroke end x: ", (flowerTop.x - flowerTop.width / 2) + flowerTop.width)//240.5+119=359.5
+    console.log("flowerTop.toLocal Point: ", flowerTop.toLocal(new Point()))//-300:-300
+    console.log("flowerTop.toGlobal(new Point()): ", flowerTop.toGlobal(new Point()))
+    console.log("flowerTop.parent.toGlobal(flowerTop.position)", flowerTop.parent.toGlobal(flowerTop.position))
+
+    const aux1 = flowerTop.parent.toLocal(new Point(600, 600))
+    // flowerTopStroke.position.copyFrom(aux);
+    console.log("aux1 = flowerTop.parent.toLocal(new Point(600, 600))\n", aux1)
+
     // graphics.setStrokeStyle({ width: 1, color: 0xff0000, alignment: 1, alpha: 1 }); // Grosor y color del contorno
     // graphics.rect((goNext.x - goNext.width / 2), (goNext.y - goNext.height / 2), goNext.width, goNext.height);
     // graphics.fill(0x0000ff);
-    goNextWithHat.addChild(goNextStroke)
+    flowerToptWithHat.addChild(flowerTopStroke)
 
     hat.anchor.set(0.5);
     // hat.scale.set(0.9, 0.9);
     hat.x = flowerTop.x;
     hat.y = flowerTop.y - (flowerTop.height / 2)
-    goNextWithHat.addChild(hat)
+    flowerToptWithHat.addChild(hat)
 
 
     const hatStroke = new Graphics()
         .rect((hat.x - hat.width / 2), (hat.y - hat.height / 2), hat.width, hat.height)
-        .stroke(0x0000ff)
-    goNextWithHat.addChild(hatStroke)
+        .stroke(0x0000ff);
+    flowerToptWithHat.addChild(hatStroke)
 
     //-------------------About Global and Local
-    console.log(hat.toGlobal(new Point()))
-    console.log(hat.parent.toGlobal(hat.position))
+    // console.log(hat.toGlobal(new Point()))
+    // console.log(hat.parent.toGlobal(hat.position))
 
     // const aux = hat.parent.toLocal(new Point(600, 600))
     // hat.position.copyFrom(aux);
     //-------------------END About Global and Local
 
 
-    app.stage.addChild(goNextWithHat)
+    app.stage.addChild(flowerToptWithHat)
 
     // let elapsed = 0.0;
     // app.ticker.add((ticker: any) => {
